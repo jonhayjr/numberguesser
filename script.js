3c7f5f8694898bb33d811a86cb9219d359f28721
jonhayjr <jonhayjr@gmail.com>
2025-05-21 21:04:16 -0600

    initialized repo

---
+++ script.js
@@ -0,0 +1,9 @@
+let humanScore = 0;
+let computerScore = 0;
+let currentRoundNumber = 1;
+
+// Write your code below:
+//Return random number between 0 and 9
+const generateTarget = () => {
+    return Math.floor(Math.random() * 10);
+}
