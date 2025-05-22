6388c109a80b1eabbb3c67caa3f4014af490c2bc
jonhayjr <jonhayjr@gmail.com>
2025-05-21 21:38:06 -0600

    Added compareGuesses function logic

--- script.js
+++ script.js
@@ -7,3 +7,21 @@ let currentRoundNumber = 1;
 const generateTarget = () => {
     return Math.floor(Math.random() * 10);
 }
+
+const targetNumber = generateTarget();
+
+//
+const compareGuesses = (humanGuess, computerGuess, targetNumber) => {
+    //Find difference between targe and guess value and convert to absolute number
+    const humanTargetDiff = Math.abs(humanGuess - targetNumber);
+    const computerTargetDiff = Math.abs(computerGuess - targetNumber);
+    
+    //Return true if humanTargetDiff is less than or equal to computerTargetDiff and false otherwise
+    if (humanTargetDiff <= computerTargetDiff) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+console.log(compareGuesses(1, 7, targetNumber));
