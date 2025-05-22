b169a377710f4d34f19775c991ba98ffa8628950
jonhayjr <jonhayjr@gmail.com>
2025-05-21 21:49:57 -0600

    Made generateTarget logic more concise

--- script.js
+++ script.js
@@ -4,9 +4,7 @@ let currentRoundNumber = 1;
 
 // Write your code below:
 //Return random number between 0 and 9
-const generateTarget = () => {
-    return Math.floor(Math.random() * 10);
-}
+const generateTarget = () => Math.floor(Math.random() * 10);
 
 const targetNumber = generateTarget();
 
